37632c0b0a8bd356a318ca07e1ccfbd8659bd980
New-Skytech <76553267+New-Skytech@users.noreply.github.com>
2022-12-10 01:28:43 +0500

    Add files via upload

---
+++ strings/strings/strings.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include<fstream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("INPUT.txt");
+    ofstream fout;
+    fout.open("OUTPUT.txt");
+    string s, s1;
+    int k = 0;
+    fin >> s >> s1;
+    set<string> a;
+    for (int i = 0; i < s1.size(); ++i) {
+        a.insert(s1.substr(s1.size() - i - 1) + s1.substr(0, s1.size() - i - 1));
+    }
+    for (auto i : a) {
+        int pos = s.find(i);
+        while (pos + 1) {
+            ++k;
+            pos = s.find(i, pos + 1);
+        }
+    }
+    fout << k;
+    fout.close();
+    fin.close();
+}
---
+++ Телеграфный перевод/Телеграфный перевод/Телеграфный перевод.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ifstream fin;
+	fin.open("INPUT.txt");
+	ofstream fout;
+	fout.open("OUTPUT.txt");
+	int tranzaction, count;
+
+	fin >> tranzaction;
+
+	if (tranzaction % 100 == 0)
+	{
+		count = tranzaction / 100;
+	}
+	else
+	{
+		count = tranzaction / 100 + 1;
+	}
+
+	if (tranzaction % 100 > count * 7)
+	{
+		if (tranzaction % 100 > (count + 1) * 7)
+		{
+			cout << (tranzaction - count * 7);
+		}
+		else
+		{
+			cout << (tranzaction - (count - 1) * 7);
+		}
+	}
+	else
+	{
+		if (tranzaction % 100 > (count * 7))
+		{
+			cout << tranzaction - (count - 1) * 7;
+		}
+		else
+		{
+			cout << ((count - 1) * 100);
+		}
+	}
+	fout.close();
+	fin.close();
+}
